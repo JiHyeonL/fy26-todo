8667ea133c73e3ce4ce9a0b29e4dd956d7fd2676
lemone <lejinhy@gmail.com>
2025-11-13 01:45:49 +0900

    feat: User -> Member 네이밍 변경 및 사용자 정보 필드(아이디, 비밀번호) 추가

---
+++ src/main/java/com/fy26/todo/domain/Member.java
@@ -0,0 +1,40 @@
+package com.fy26.todo.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+public class Member extends TimeStamp {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "role")
+    private Role role;
+
+    @Column(name = "member_id")
+    private String memberId;
+
+    @Column(name = "password")
+    private String password;
+
+    public Member(Role role, String memberId, String password) {
+        this.role = role;
+        this.memberId = memberId;
+        this.password = password;
+    }
+
+    public Member(Long id, Role role, String memberId, String password) {
+        this.id = id;
+        this.role = role;
+        this.memberId = memberId;
+        this.password = password;
+    }
+}
