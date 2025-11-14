e765a3bb4e6cdd796bd192be99ff11a068353452
lemone <lejinhy@gmail.com>
2025-11-15 02:12:25 +0900

    refactor: TodoGetResponse 정적 팩토리 메서드 추가

--- src/main/java/com/fy26/todo/dto/todo/TodoGetResponse.java
+++ src/main/java/com/fy26/todo/dto/todo/TodoGetResponse.java
@@ -1,8 +1,24 @@
 package com.fy26.todo.dto.todo;
 
+import com.fy26.todo.domain.Tag;
+import com.fy26.todo.domain.Todo;
 import com.fy26.todo.dto.tag.TagGetResponse;
 import java.time.LocalDateTime;
 import java.util.List;
 
 public record TodoGetResponse(long id, long orderIndex, String content, List<TagGetResponse> tags, boolean completed, LocalDateTime dueDate, long dDay) {
+
+    public static TodoGetResponse of(final Todo todo, final long dDay, final List<Tag> tags) {
+        return new TodoGetResponse(
+                todo.getId(),
+                todo.getOrderIndex(),
+                todo.getContent(),
+                tags.stream()
+                    .map(tag -> new TagGetResponse(tag.getId(), tag.getName()))
+                    .toList(),
+                todo.isCompleted(),
+                todo.getDueDate(),
+                dDay
+        );
+    }
 }
