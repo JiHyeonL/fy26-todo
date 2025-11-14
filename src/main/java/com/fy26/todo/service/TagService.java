1968713d9a1cf9b0a58b036c20982d05285e48fb
lemone <lejinhy@gmail.com>
2025-11-14 17:14:59 +0900

    refactor: 투두 반환 로직 최적화

--- src/main/java/com/fy26/todo/service/TagService.java
+++ src/main/java/com/fy26/todo/service/TagService.java
@@ -3,15 +3,11 @@ package com.fy26.todo.service;
 import com.fy26.todo.domain.Tag;
 import com.fy26.todo.domain.Todo;
 import com.fy26.todo.domain.TodoTag;
-import com.fy26.todo.exception.TagErrorCode;
-import com.fy26.todo.exception.TagException;
 import com.fy26.todo.repository.TagRepository;
 import com.fy26.todo.repository.TodoRepository;
 import com.fy26.todo.repository.TodoTagRepository;
 import jakarta.transaction.Transactional;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -38,12 +34,8 @@ public class TagService {
 
     public List<Tag> getTagsForTodo(final Long todoId) {
         final List<TodoTag> todoTags = todoTagRepository.findAllByTodoId(todoId);
-        final List<Tag> tags = new ArrayList<>();
-        for (final TodoTag todoTag : todoTags) {
-            Tag tag = tagRepository.findById(todoTag.getTag().getId())
-                    .orElseThrow(() -> new TagException(TagErrorCode.TAG_NOT_FOUND, Map.of("tagId", todoTag.getTag())));
-            tags.add(tag);
-        }
-        return tags;
+        return todoTags.stream()
+                .map(TodoTag::getTag)
+                .toList();
     }
 }
