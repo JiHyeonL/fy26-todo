82f92a8423e1093c825c286d762a0e3813f369c1
lemone <lejinhy@gmail.com>
2025-11-14 23:09:49 +0900

    test: 투두 서비스 응답 변경에 따른 테스트 코드 수정

--- src/test/java/com/fy26/todo/service/TodoServiceTest.java
+++ src/test/java/com/fy26/todo/service/TodoServiceTest.java
@@ -14,6 +14,7 @@ import com.fy26.todo.domain.TodoPosition;
 import com.fy26.todo.dto.tag.TagCreateRequest;
 import com.fy26.todo.dto.tag.TagCreateResponse;
 import com.fy26.todo.dto.todo.TodoCreateRequest;
+import com.fy26.todo.dto.todo.TodoCreateResponse;
 import com.fy26.todo.dto.todo.TodoGetResponse;
 import com.fy26.todo.dto.todo.TodoOrderUpdateRequest;
 import com.fy26.todo.dto.todo.TodoUpdateRequest;
@@ -57,10 +58,10 @@ class TodoServiceTest {
         memberRepository.save(member);
 
         // when
-        final Todo actual = todoService.createTodo(request, member);
+        final TodoCreateResponse actual = todoService.createTodo(request, member);
 
         // then
-        assertThat(actual.getOrderIndex()).isEqualTo(INITIAL_ORDER_INDEX);
+        assertThat(actual.orderIndex()).isEqualTo(INITIAL_ORDER_INDEX);
     }
 
     @DisplayName("todo 생성 시 이미 존재하는 태그 이름이 있다면 제외하고 태그를 생성한다.")
@@ -71,16 +72,16 @@ class TodoServiceTest {
         memberRepository.save(member);
         final List<String> tags = List.of("태그1", "태그2", "태그3");
         final TodoCreateRequest firstRequest = new TodoCreateRequest("첫 번째 할 일", List.of(tags.get(0), tags.get(1)), LocalDateTime.now());
-        final Todo firstTodo = todoService.createTodo(firstRequest, member);
+        final TodoCreateResponse firstTodo = todoService.createTodo(firstRequest, member);
         final TodoCreateRequest secondRequest = new TodoCreateRequest("두 번째 할 일", List.of(tags.get(1), tags.get(2)), LocalDateTime.now());
 
         // when
-        final Todo secondTodo = todoService.createTodo(secondRequest, member);
+        final TodoCreateResponse secondTodo = todoService.createTodo(secondRequest, member);
 
         // then
         assertThat(tagRepository.findAllByMemberAndNameIn(member, tags)).hasSize(tags.size());
-        assertThat(todoTagRepository.findAllByTodoId(firstTodo.getId())).hasSize(firstRequest.tagNames().size());
-        assertThat(todoTagRepository.findAllByTodoId(secondTodo.getId())).hasSize(secondRequest.tagNames().size());
+        assertThat(todoTagRepository.findAllByTodoId(firstTodo.id())).hasSize(firstRequest.tagNames().size());
+        assertThat(todoTagRepository.findAllByTodoId(secondTodo.id())).hasSize(secondRequest.tagNames().size());
     }
 
     @DisplayName("todo를 두 개 생성한다.")
@@ -92,12 +93,12 @@ class TodoServiceTest {
         memberRepository.save(member);
 
         // when
-        final Todo firstActual = todoService.createTodo(request, member);
-        final Todo secondActual = todoService.createTodo(request, member);
+        final TodoCreateResponse firstActual = todoService.createTodo(request, member);
+        final TodoCreateResponse secondActual = todoService.createTodo(request, member);
 
         // then
-        assertThat(firstActual.getOrderIndex()).isEqualTo(INITIAL_ORDER_INDEX);
-        assertThat(secondActual.getOrderIndex()).isEqualTo(INITIAL_ORDER_INDEX + GAP_ORDER_INDEX);
+        assertThat(firstActual.orderIndex()).isEqualTo(INITIAL_ORDER_INDEX);
+        assertThat(secondActual.orderIndex()).isEqualTo(INITIAL_ORDER_INDEX + GAP_ORDER_INDEX);
     }
 
     @DisplayName("태그 추가 생성 시 사용자가 다르면 중복 태그 이름을 허용한다.")
@@ -130,7 +131,7 @@ class TodoServiceTest {
         todoService.createTodo(request, member);
 
         // when
-        final var todos = todoService.getTodos(member);
+        final List<TodoGetResponse> todos = todoService.getTodos(member);
 
         // then
         assertThat(todos).hasSize(2);
@@ -143,10 +144,10 @@ class TodoServiceTest {
         final TodoCreateRequest request = new TodoCreateRequest("첫 번째 할 일", Collections.emptyList(), LocalDateTime.now());
         final Member member = new Member(Role.USER, "아이디", "비번");
         memberRepository.save(member);
-        final Todo savedTodo = todoService.createTodo(request, member);
+        final TodoCreateResponse savedTodo = todoService.createTodo(request, member);
 
         // when & then
-        assertThatCode(() -> todoService.getTodo(savedTodo.getId())).doesNotThrowAnyException();
+        assertThatCode(() -> todoService.getTodo(savedTodo.id())).doesNotThrowAnyException();
     }
 
     @DisplayName("존재하지 않는 id로 todo를 조회할 시 예외가 발생한다.")
@@ -157,7 +158,7 @@ class TodoServiceTest {
         final Member member = new Member(Role.USER, "아이디", "비번");
         memberRepository.save(member);
         final long invalidId = todoService.createTodo(request, member)
-                .getId() + 1;
+                .id() + 1;
 
         // when & then
         assertThatThrownBy(() -> todoService.getTodo(invalidId)).isExactlyInstanceOf(TodoException.class);
@@ -171,10 +172,10 @@ class TodoServiceTest {
         final Member invalidMember = new Member(Role.USER, "아이디1", "비번1");
         final Member validMember = new Member(Role.USER, "아이디2", "비번2");
         memberRepository.saveAll(List.of(invalidMember, validMember));
-        final Todo savedTodo = todoService.createTodo(request, validMember);
+        final TodoCreateResponse savedTodo = todoService.createTodo(request, validMember);
 
         // when & then
-        final long todoId = savedTodo.getId();
+        final long todoId = savedTodo.id();
         final TodoOrderUpdateRequest todoOrderRequest = new TodoOrderUpdateRequest(TodoPosition.TOP, null, null);
         assertThatThrownBy(() -> todoService.updateTodoOrder(todoId, todoOrderRequest, invalidMember))
                 .isExactlyInstanceOf(TodoException.class);
@@ -187,15 +188,15 @@ class TodoServiceTest {
         final TodoCreateRequest request = new TodoCreateRequest("할 일", Collections.emptyList(), LocalDateTime.now());
         final Member member = new Member(Role.USER, "아이디", "비번");
         memberRepository.save(member);
-        final Todo firstTodo = todoService.createTodo(request, member);
-        final Todo secondTodo = todoService.createTodo(request, member);
+        final TodoCreateResponse firstTodo = todoService.createTodo(request, member);
+        final TodoCreateResponse secondTodo = todoService.createTodo(request, member);
 
         // when
-        todoService.updateTodoOrder(secondTodo.getId(),
-                new TodoOrderUpdateRequest(TodoPosition.TOP, null, firstTodo.getId()), member);
+        todoService.updateTodoOrder(secondTodo.id(),
+                new TodoOrderUpdateRequest(TodoPosition.TOP, null, firstTodo.id()), member);
 
         // then
-        final TodoGetResponse secondTodoInfo = todoService.getTodo(secondTodo.getId());
+        final TodoGetResponse secondTodoInfo = todoService.getTodo(secondTodo.id());
         assertThat(secondTodoInfo.orderIndex()).isEqualTo(INITIAL_ORDER_INDEX - GAP_ORDER_INDEX);
     }
 
@@ -206,16 +207,16 @@ class TodoServiceTest {
         final TodoCreateRequest request = new TodoCreateRequest("할 일", Collections.emptyList(), LocalDateTime.now());
         final Member member = new Member(Role.USER, "아이디", "비번");
         memberRepository.save(member);
-        final Todo firstTodo = todoService.createTodo(request, member);
-        final Todo secondTodo = todoService.createTodo(request, member);
+        final TodoCreateResponse firstTodo = todoService.createTodo(request, member);
+        final TodoCreateResponse secondTodo = todoService.createTodo(request, member);
 
         // when
-        todoService.updateTodoOrder(firstTodo.getId(),
-                new TodoOrderUpdateRequest(TodoPosition.BOTTOM, secondTodo.getId(), null), member);
+        todoService.updateTodoOrder(firstTodo.id(),
+                new TodoOrderUpdateRequest(TodoPosition.BOTTOM, secondTodo.id(), null), member);
 
         // then
-        final TodoGetResponse firstTodoInfo = todoService.getTodo(firstTodo.getId());
-        assertThat(firstTodoInfo.orderIndex()).isEqualTo(secondTodo.getOrderIndex() + GAP_ORDER_INDEX);
+        final TodoGetResponse firstTodoInfo = todoService.getTodo(firstTodo.id());
+        assertThat(firstTodoInfo.orderIndex()).isEqualTo(secondTodo.orderIndex() + GAP_ORDER_INDEX);
     }
 
     @DisplayName("첫번째 todo 위치를 두번째 todo와 세번째 todo의 사이로 변경한다")
@@ -225,17 +226,17 @@ class TodoServiceTest {
         final TodoCreateRequest request = new TodoCreateRequest("할 일", Collections.emptyList(), LocalDateTime.now());
         final Member member = new Member(Role.USER, "아이디", "비번");
         memberRepository.save(member);
-        final Todo firstTodo = todoService.createTodo(request, member);
-        final Todo secondTodo = todoService.createTodo(request, member);
-        final Todo thirdTodo = todoService.createTodo(request, member);
+        final TodoCreateResponse firstTodo = todoService.createTodo(request, member);
+        final TodoCreateResponse secondTodo = todoService.createTodo(request, member);
+        final TodoCreateResponse thirdTodo = todoService.createTodo(request, member);
 
         // when
-        todoService.updateTodoOrder(firstTodo.getId(),
-                new TodoOrderUpdateRequest(TodoPosition.MIDDLE, secondTodo.getId(), thirdTodo.getId()), member);
+        todoService.updateTodoOrder(firstTodo.id(),
+                new TodoOrderUpdateRequest(TodoPosition.MIDDLE, secondTodo.id(), thirdTodo.id()), member);
 
         // then
-        final TodoGetResponse firstTodoInfo = todoService.getTodo(firstTodo.getId());
-        assertThat(firstTodoInfo.orderIndex()).isEqualTo((secondTodo.getOrderIndex() + thirdTodo.getOrderIndex()) / 2);
+        final TodoGetResponse firstTodoInfo = todoService.getTodo(firstTodo.id());
+        assertThat(firstTodoInfo.orderIndex()).isEqualTo((secondTodo.orderIndex() + thirdTodo.orderIndex()) / 2);
     }
 
     @DisplayName("todo 순서 변경 시 Long 범위를 벗어날 경우 재정렬을 수행한다.")
@@ -289,13 +290,13 @@ class TodoServiceTest {
         final TodoCreateRequest request = new TodoCreateRequest("첫 번째 할 일", Collections.emptyList(), LocalDateTime.now());
         final Member member = new Member(Role.USER, "아이디", "비번");
         memberRepository.save(member);
-        final Todo savedTodo = todoService.createTodo(request, member);
+        final TodoCreateResponse savedTodo = todoService.createTodo(request, member);
 
         // when
-        todoService.updateComplete(savedTodo.getId(), member);
+        todoService.updateComplete(savedTodo.id(), member);
 
         // then
-        final TodoGetResponse todoInfo = todoService.getTodo(savedTodo.getId());
+        final TodoGetResponse todoInfo = todoService.getTodo(savedTodo.id());
         assertThat(todoInfo.completed()).isTrue();
     }
 
@@ -306,15 +307,15 @@ class TodoServiceTest {
         final TodoCreateRequest request = new TodoCreateRequest("첫 번째 할 일", Collections.emptyList(), LocalDateTime.now());
         final Member member = new Member(Role.USER, "아이디", "비번");
         memberRepository.save(member);
-        final Todo savedTodo = todoService.createTodo(request, member);
+        final TodoCreateResponse savedTodo = todoService.createTodo(request, member);
 
         // when
         final String expected = "수정된 할 일";
-        todoService.updateTodo(savedTodo.getId(),
+        todoService.updateTodo(savedTodo.id(),
                 new TodoUpdateRequest(expected, null), member);
 
         // then
-        final TodoGetResponse actual = todoService.getTodo(savedTodo.getId());
+        final TodoGetResponse actual = todoService.getTodo(savedTodo.id());
         assertThat(actual.content()).isEqualTo(expected);
     }
 
@@ -325,10 +326,10 @@ class TodoServiceTest {
         final TodoCreateRequest request = new TodoCreateRequest("첫 번째 할 일", Collections.emptyList(), LocalDateTime.now());
         final Member member = new Member(Role.USER, "아이디", "비번");
         memberRepository.save(member);
-        final Todo savedTodo = todoService.createTodo(request, member);
+        final TodoCreateResponse savedTodo = todoService.createTodo(request, member);
 
         // when & then
-        final long todoId = savedTodo.getId();
+        final long todoId = savedTodo.id();
         final LocalDateTime pastDueDate = LocalDateTime.now().minusDays(1);
         final TodoUpdateRequest updateRequest = new TodoUpdateRequest(null, pastDueDate);
         assertThatThrownBy(() -> todoService.updateTodo(todoId, updateRequest, member))
@@ -343,11 +344,11 @@ class TodoServiceTest {
                 LocalDateTime.now());
         final Member member = new Member(Role.USER, "아이디", "비번");
         memberRepository.save(member);
-        final Todo savedTodo = todoService.createTodo(request, member);
+        final TodoCreateResponse savedTodo = todoService.createTodo(request, member);
         final TagCreateRequest tagCreateRequest = new TagCreateRequest(List.of("태그1", "태그2"));
 
         // when
-        final List<TagCreateResponse> responses = todoService.addTags(savedTodo.getId(), tagCreateRequest, member);
+        final List<TagCreateResponse> responses = todoService.addTags(savedTodo.id(), tagCreateRequest, member);
 
         // then
         assertThat(responses).hasSize(2);
@@ -361,13 +362,13 @@ class TodoServiceTest {
                 LocalDateTime.now());
         final Member member = new Member(Role.USER, "아이디", "비번");
         memberRepository.save(member);
-        final Todo savedTodo = todoService.createTodo(request, member);
+        final TodoCreateResponse savedTodo = todoService.createTodo(request, member);
 
         // when
-        todoService.deleteTodo(savedTodo.getId(), member);
+        todoService.deleteTodo(savedTodo.id(), member);
 
         // then
-        final long todoId = savedTodo.getId();
+        final long todoId = savedTodo.id();
         assertThatThrownBy(() -> todoService.getTodo(todoId))
                 .isExactlyInstanceOf(TodoException.class);
     }
@@ -380,10 +381,10 @@ class TodoServiceTest {
                 LocalDateTime.now());
         final Member member = new Member(Role.USER, "아이디", "비번");
         memberRepository.save(member);
-        final Todo savedTodo = todoService.createTodo(request, member);
+        final TodoCreateResponse savedTodo = todoService.createTodo(request, member);
 
         // when
-        todoService.deleteTodo(savedTodo.getId(), member);
+        todoService.deleteTodo(savedTodo.id(), member);
         final List<TodoGetResponse> todos = todoService.getTodos(member);
 
         // then
