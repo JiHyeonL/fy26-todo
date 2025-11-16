91aee32de1b5ea176dc7a209540567732089eb6a
lemone <lejinhy@gmail.com>
2025-11-16 21:26:06 +0900

    feat: 태그가 null일 때 예외 메세지 추가

--- src/main/java/com/fy26/todo/dto/tag/TagCreateRequest.java
+++ src/main/java/com/fy26/todo/dto/tag/TagCreateRequest.java
@@ -4,7 +4,7 @@ import jakarta.validation.constraints.NotNull;
 import java.util.List;
 
 public record TagCreateRequest(
-        @NotNull
+        @NotNull(message = "태그 리스트는 null일 수 없습니다.")
         List<String> tagNames
 ) {
 }
